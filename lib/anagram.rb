0384b5598f8f63e988382d29e7d38ae060607df4
John Garbero <john.garbero.itb@gmail.com>
2022-06-28 16:05:37 -0500

    commit tree 8503a92fb748f25ad42283695db257bc81e45431

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+
+class Anagram
+    
+
+    def initialize (word)
+        @word=word
+    end
+
+
+end
+
+#take a word
+#turn it into characters
+#reverse those characters
+#compare with original
